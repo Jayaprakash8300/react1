487b38843d8487008bad86420a63998da805259f
Jayaprakash8300 <115724410+Jayaprakash8300@users.noreply.github.com>
2022-12-21 10:21:19 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './Header.css';
+
+const Header = function () {
+    // const headerStyle={
+    //     textAlign: 'center',
+    //     padding: 20, 
+    //     background:'aqua', 
+    //     color: 'green',
+    //     textTransform: 'uppercase'
+    // };
+    
+    return (
+        <div className="header">
+        Phone Book Application
+      </div>
+    )
+}
+
+export default Header;
